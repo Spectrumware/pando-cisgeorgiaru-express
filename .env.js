92ce7a891e698fa06c3305bb658f309906068a2e
Dwight Mcleish Jr <102700723+Spectrum-Dwight@users.noreply.github.com>
2025-10-24 12:39:16 -0500

    Add files via upload

---
+++ .env.js
@@ -0,0 +1,11 @@
+
+module.exports = /** @type {const} */({
+    /** @type {'development' | 'testing' | 'production'} */
+    NODE_ENV: 'development',
+    app: {
+        domain: 'http://localhost:3000',
+        topDomain: 'cisgeorgiaru.org',
+        title: 'Reality U CIS Georgia',
+        workerID: 1,
+    }
+});
